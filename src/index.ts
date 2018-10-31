2ad687288d4245f46db2abec5f38ab3173ae569f
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2018-10-31 19:48:23 -0300

    thinking....

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 
 export interface Type<T> extends Function {
-    new (...args: any[]): T
+    new(...args: any[]): T
 }
-
-export interface Parser<E> {
-    mutate(rawData: any): E;
-}
-
 
 export interface FunctionalParser<E> {
     (any): E;
 }
 
+export interface ParserRepository {
+    [name: string]: FunctionalParser<any>;
+}
+
+export interface Parser<E> {
+    mutate(rawData: any, parserRepository?: ParserRepository): E;
+}
+
 export interface ObjectParserRecipe<E> {
-    target: Type<E> | FunctionalParser<E>;
-    nestedTargets: { [fieldName: string]: ParserRecipe<any> };
+    target: Type<E> | FunctionalParser<E> | string;
+    nestedTargets?: { [fieldName: string]: ParserRecipe<any> | string };
+    reposiroty?: ParserRepository;
 }
 
 export type ParserRecipe<E> = ObjectParserRecipe<E> | Type<E> | FunctionalParser<E>;
